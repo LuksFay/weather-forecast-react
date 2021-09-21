8fcdd0add82542ae9e57f9755a4df55f4efe253c
luksfay <lukspicco@hotmail.com>
2021-09-21 19:06:38 -0300

    arreglado el https

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ useEffect(()=>{
   const consultarAPI = async () => {
     if(consultar){
       const appId = 'd2c3a084c46685cd08aa05ea80e59ae0';
-    const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
     
     const respuesta = await fetch (url);
     const resultado = await respuesta.json();
